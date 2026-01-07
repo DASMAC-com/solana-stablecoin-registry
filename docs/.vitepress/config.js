f652f269c16b0f86342992a81eb5986b1b521aa6
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-07 15:12:56 -0800

    chore(ENG-142): Add OpenGraph site tags

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,20 @@ export default {
   title: "Solana Stablecoin Registry",
   description:
     "Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)",
-  head: [["meta", { property: "og:site_name", content: "DASMAC" }]],
+  head: [
+    ["meta", { property: "og:site_name", content: "DASMAC" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:url", content: "https://stables.dasmac.com/" }],
+    ["meta", { property: "og:title", content: "Solana Stablecoin Registry" }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content:
+          "Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)",
+      },
+    ],
+  ],
   srcDir: "src",
   markdown: {
     lineNumbers: true,
