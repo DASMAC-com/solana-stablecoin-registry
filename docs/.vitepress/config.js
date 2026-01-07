e0ff0ba49306e8290f33671f6ef483441c00cdc7
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-07 14:03:46 -0800

    docs(ENG-136): Initialize VitePress site

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+export default {
+  title: "DASMAC Stablecoin Registry",
+  srcDir: "src",
+  markdown: {
+    lineNumbers: true,
+    math: true,
+  },
+  themeConfig: {
+    sidebar: [
+      { text: "Index", link: "/" },
+    ],
+  },
+};
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+# DASMAC Stablecoin Registry
+
+<!-- markdownlint-disable MD013 -->
+
+> [!important] Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)
+
+<!-- markdownlint-enable MD013 -->
