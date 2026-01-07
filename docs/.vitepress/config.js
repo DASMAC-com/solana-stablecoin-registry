c87278b51e08450f9b590ce160002183d2d00747
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-07 15:08:56 -0800

    chore(ENG-141): Update site name metadata (#4)
    Update OpenGraph site name property

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ export default {
   title: "Solana Stablecoin Registry",
   description:
     "Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)",
+  head: [["meta", { property: "og:site_name", content: "DASMAC" }]],
   srcDir: "src",
   markdown: {
     lineNumbers: true,
